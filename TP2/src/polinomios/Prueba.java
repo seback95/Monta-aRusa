99c1000ff643546c3bfe446ffaabcf17dfd5244f
seba9 <seba.95.95@hotmail.com>
2018-05-08 15:02:23 -0300

    Hecho ejercicio 2.1.1 y 2.1.2

---
+++ TP2/src/polinomios/Prueba.java
@@ -0,0 +1,17 @@
+package polinomios;
+
+public class Prueba {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Polinomio poli=new Polinomio(3);
+		poli.cargarPOli();
+		poli.verPoli();
+		System.out.println(poli.evaluarRecursiva(2));
+		
+		
+	
+		
+	}
+
+}
